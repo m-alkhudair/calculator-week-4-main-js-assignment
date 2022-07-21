b9b7a3cc81d27637d45face0c79fd48d7470f409
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2022-07-21 10:09:33 +0300

    calculator logic

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const inputDisplay = document.querySelector('input');
+const allButtons = document.querySelectorAll('button');
+
 // Math operator functions
 const add = (...args) => {
     let sum = 0;
@@ -7,45 +10,67 @@ const add = (...args) => {
     return sum;
 };
 
-console.log(add(1,3,5));
-
 const subtract = (...args) => {
     const subtraction = args.reduce((prevVal, curVal) => prevVal-curVal);
 
     return subtraction;
 };
 
-console.log(subtract(7,6,1,1));
-
 const multiplay = (...args) => {
     const multiplication = args.reduce((prevVal, curVal) => prevVal*curVal);
     return multiplication;
 };
-
-console.log(multiplay(7,6,1,1,3));
 
 const divide = (...args) => {
     const division = args.reduce((prevVal, curVal) => prevVal/curVal);
     return division;
 };
 
-console.log(divide(4,2));
-
 const percentage = (number) => {
     return number/100;
 };
-
-console.log(percentage(4));
 
 const toggleSign = (number) => {
     return number*(-1);
 };
 
-console.log(toggleSign(-4));
+const square = (arg) => {
+    return arg**2;
+}
+
+console.log(square(9));
+
+const squareRoot = (arg) => {
+    return arg**(1/2);
+}
 
 const operate = (operator, num1, num2) => {
     return operator(num1, num2);
 }
+
+// inputDisplay.placeholder = 'testing';
+inputDisplay.value = inputDisplay.placeholder;
+
+// console.log(inputDisplay.value);
+
+const inputToCalculator = (e) => {
+    console.log(e.target.textContent);
+    // let display;
+    // display += e.target.textContent; 
+
+    // for (const item of display) {
+    //     switch (item) {
+    //         case item ==
+    //     }
+    // }
+    // inputDisplay.placeholder += e.target.textContent;
+};
+
+for (const button of allButtons) {
+    button.addEventListener('click', inputToCalculator);
+};
+
+console.log(inputDisplay);
 
 console.log(operate(add, 1, 3));
 console.log(operate(subtract, 1, 3));
