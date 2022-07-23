17d4481c642e7e2482cf956bb1255945c2b9c414
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2022-07-23 14:39:15 +0300

    Cleaning up script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ let operation = undefined;
 let currentValue = ''
 let previousValue = ''
 
+let dividedByZero = false;
+let toClear = false;
+
 const inputDisplay = document.querySelector('input');
 inputDisplay.value = inputDisplay.placeholder;
 const numberButtons = document.querySelectorAll('.number');
@@ -16,7 +19,6 @@ const leftBracketButton = document.getElementById('leftBracket');
 const rightBracketButton = document.getElementById('rightBracket');
 const deleterButton = document.getElementById('del');
 
-// Math operator functions
 const add = (...args) => {
     let sum = 0;
     for (const argument of args) {
@@ -39,8 +41,6 @@ const multiply = (...args) => {
 const divide = (arg1, arg2) => {
     return parseFloat(arg1)/parseFloat(arg2);
 };
-
-let dividedByZero = false;
 
 const operate = () => {
     console.log(previousValue);
@@ -76,39 +76,16 @@ const operate = () => {
     console.log(firstArg);
     console.log(secondArg);
 
-    // if (calculation.toString().includes('.')) {
-    //     calculation = parseFloat(calculation).toFixed(14)
-    // }
-
     currentValue = calculation;
     operation = undefined;
     previousValue = ''
 }
 
 const appendNumber = (e) => {
-    // if (currentValue === '.' && currentValue.includes('.')) return;
-
-
-    // DID THIS HELP?
-    // if(e.target.textContent === '.' && currentValue == ''){
-    //     currentValue = '0' + '.';
-    // }
-
-    // if (e.target.textContent === '.' && currentValue) {
-    //     if(currentValue === '.') {
-    //         currentValue = '0' + '.';
-    //     } else {
-    //         currentValue = currentValue + '.'
-    //     }
-    // }
-
     let inputNumber = e.key? e.key : e.target.textContent;
 
-    
-    
     if (inputNumber === '.' && currentValue.toString().includes('.')) return;
-    
-    
+     
     if(toClear) {
         currentValue='';
         toClear=false;
@@ -124,7 +101,6 @@ const appendNumber = (e) => {
         }
     }
     currentValue = currentValue.toString() + inputNumber.toString();
-    // inputDisplay.value = parseFloat(currentValue);
     console.log(currentValue);
 }
 
@@ -148,21 +124,8 @@ const updateDisplayValue = () => {
     return;
    }
 
-//    if (isNaN(inputDisplay.value) || currentValue) {
-//     inputDisplay.value = '';
-//     previousValue = '';
-//     currentValue = '';
-//     operation = undefined;
-  
-//    }
-
    console.log('current vlaue: ' + currentValue);
    console.log('previous value: '+ previousValue);
-   
-    // inputDisplay.value = currentValue;
-    // if (currentValue === 0 || currentValue === '0') inputDisplay.value = '0';
-
-    // inputDisplay.value = currentValue? currentValue : previousValue;
 
    if (currentValue === '') {
         inputDisplay.value = previousValue;
@@ -171,14 +134,6 @@ const updateDisplayValue = () => {
    } else {
         inputDisplay.value = currentValue;
    }
-
-    // if (isNaN(inputDisplay.value)) {
-        
-    // }
-    // if (currentValue == '.') {
-    //     // console.log(true);
-    //     currentValue = '0' + '.'
-    // }
 
     if (inputDisplay.value.toString().includes('.') &&  decimalCount(inputDisplay.value) > 14) {
         inputDisplay.value = parseFloat(inputDisplay.value).toFixed(14)
@@ -197,13 +152,8 @@ const decimalCount = (number) =>{
     return 0;
 }
 
-let decimalDot = false;
 numberButtons.forEach(button => {
     button.addEventListener('click', (e)=> {
-        // if (button.id === 'dot') {
-        //     console.log('dot dot');
-        //     decimalDot = true;
-        // }
         appendNumber(e)
         updateDisplayValue()
     } )
@@ -216,11 +166,9 @@ operatorButtons.forEach(button=>{
     })
 })
 
-let toClear = false;
 equalsButton.addEventListener('click', (e)=>{
     operate()
     updateDisplayValue()
-    // previousValue = currentValue;
     toClear= true;
     
 })
@@ -272,14 +220,12 @@ leftBracketButton.addEventListener('click', ()=>{
     currentValue = inputDisplay.value;
     if (currentValue) {
         updateDisplayValue()
-        // console.log(currentValue);
     }
 })
 
 rightBracketButton.addEventListener('click', ()=>{
     operate()
     updateDisplayValue()
-    // previousValue = currentValue;
     toClear= true;
 
 })
@@ -299,10 +245,7 @@ const numberKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const operationKeys = ['+', '-', '*', '/'];
 
 document.addEventListener('keypress', (e)=>{
-    console.log(e.key);
-    console.log(e);
     if (numberKeys.includes(e.key)) {
-        // console.log(e.key);
         appendNumber(e)
         updateDisplayValue()
 
@@ -316,7 +259,6 @@ document.addEventListener('keypress', (e)=>{
     if (e.key === 'Enter') {
         operate()
         updateDisplayValue()
-        // previousValue = currentValue;
         toClear= true;
     
     }
